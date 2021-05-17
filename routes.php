22e6f8a8ad91116fcd18eb326d2a4fc80a5b7c09
Usvaandr <andrius.usvaltas@gmail.com>
2021-05-17 23:47:32 +0300

    Refactored the currect code. Created form on the website and associated a URI with the request type (GET, POST).

--- routes.php
+++ routes.php
@@ -2,9 +2,19 @@
 
 //setting up routes that will specify what is the association betweet uri and a controller.
 
-$router->define([
-    '' => 'controllers/index.php',
-    'about' => 'controllers/about.php',
-    'about/culture' => 'controllers/about-culture.php',
-    'contact' => 'controllers/contact.php'
-]);
+// $router->define([
+//     '' => 'controllers/index.php',
+//     'about' => 'controllers/about.php',
+//     'about/culture' => 'controllers/about-culture.php',
+//     'contact' => 'controllers/contact.php',
+//     'names' => 'controllers/add-name.php' //only for POST
+// ]);
+
+
+
+$router->get('', 'controllers/index.php');
+$router->get('about', 'controllers/about.php');
+$router->get('about/culture', 'controllers/about-culture.php');
+// $router->get('contact', 'controllers/contact.php',);
+
+$router->post('names', 'controllers/add-name.php');
