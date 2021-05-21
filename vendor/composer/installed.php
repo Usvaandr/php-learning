13961df64c7ddcdedb8a6d2f5edf8ef326c9704c
Usvaandr <andrius.usvaltas@gmail.com>
2021-05-21 22:04:11 +0300

    Instaed of PHP requires in the boostrap file, Composer was installed and Autoloading enabled with a single require to vendor/autoload.php file.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4b53a9b37935d5744cdbc4f6218877f05079b540',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b53a9b37935d5744cdbc4f6218877f05079b540',
+    ),
+  ),
+);
